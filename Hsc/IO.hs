e51e6ced058f9225b38fd10c88bd5d61d89ab5ca
Rohan Drape <rd@slavepianos.org>
2005-11-02 12:22:30 +0000

    In UGen

--- Hsc/IO.hs
+++ Hsc/IO.hs
@@ -3,4 +3,10 @@ module Hsc.IO where
 import Hsc.UGen
 import Hsc.MCE
 
+in' n r bus = UGen r "In" [bus] (replicate n r) 0 0
+in1 = in' 1
+in2 = in' 2
+in4 = in' 4
+in8 = in' 8
+
 out r b i = UGen r "Out" (b : (forceMCE i)) [] 0 0
