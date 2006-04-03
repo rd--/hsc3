16e81300b265799c33732028c8e3f43f8eb74630
haskore <haskore@henning-thielemann.de>
2006-04-03 10:54:14 +0000

    U8v: added ST type signatures, this makes this module compatible with Hugs

--- Hsc/U8v.hs
+++ Hsc/U8v.hs
@@ -4,8 +4,8 @@ import Data.Int (Int32, Int64)
 import Data.Word (Word8)
 import Data.Bits (Bits, shiftL, shiftR, (.&.))
 import Data.Char (chr, ord)
-import Control.Monad.ST (runST)
-import Data.Array.ST (newArray, readArray, castSTUArray)
+import Control.Monad.ST (runST, ST)
+import Data.Array.ST (STUArray, MArray, newArray, readArray, castSTUArray)
 import System.IO (openFile, IOMode(..), hPutStr, hClose)
 
 type Float32 = Float
@@ -36,17 +36,17 @@ i64_u8v n = [byte 7 n, byte 6 n, byte 5 n, byte 4 n,
 u64_u8v = i64_u8v
 
 f32_i32 :: Float32 -> Int32
-f32_i32 d = runST (do a  <- newArray (1, 1) d
-                      a' <- castSTUArray a
-                      readArray a' 1)
+f32_i32 d = runST ((do a  <- newArray (1, 1) d
+                       a' <- castSTUArray a
+                       readArray a' 1) :: ST s Int32)
 
 f32_u8v :: Float32 -> U8v
 f32_u8v f = i32_u8v (fromIntegral $ f32_i32 f)
 
 f64_i64 :: Float64 -> Int64
-f64_i64 d = runST (do a  <- newArray (1, 1) d
-                      a' <- castSTUArray a
-                      readArray a' 1)
+f64_i64 d = runST ((do a  <- newArray (1, 1) d
+                       a' <- castSTUArray a
+                       readArray a' 1) :: ST s Int64)
 
 f64_u8v :: Float64 -> U8v
 f64_u8v f = i64_u8v (fromIntegral $ f64_i64 f)
@@ -81,17 +81,17 @@ u8v_i64 [h,g,f,e,d,c,b,a] = shiftL' h 56 + shiftL' g 48 +
                             shiftL' b 8 + shiftL' a 0
 
 i32_f32 :: Int32 -> Float32
-i32_f32 d = runST (do a  <- newArray (1, 1) d
-                      a' <- castSTUArray a
-                      readArray a' 1)
+i32_f32 d = runST ((do a  <- newArray (1, 1) d
+                       a' <- castSTUArray a
+                       readArray a' 1) :: ST s Float32)
 
 u8v_f32 :: U8v -> Float32
 u8v_f32 b = i32_f32 (fromIntegral $ u8v_i32 b)
 
 i64_f64 :: Int64 -> Float64
-i64_f64 d = runST (do a  <- newArray (1, 1) d
-                      a' <- castSTUArray a
-                      readArray a' 1)
+i64_f64 d = runST ((do a  <- newArray (1, 1) d
+                       a' <- castSTUArray a
+                       readArray a' 1) :: ST s Float64)
 
 u8v_f64 :: U8v -> Float64
 u8v_f64 b = i64_f64 (fromIntegral $ u8v_i64 b)
