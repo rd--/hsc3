f253464164c7d8925c619eecc16dfb60d4d0d0ee
Rohan Drape <rd@slavepianos.org>
2006-05-21 00:56:20 +0000

    Delete example graphs

--- Hsc.hs
+++ Hsc.hs
@@ -62,24 +62,3 @@ ab = out 0 $ combn s 0.2 0.2 4
         o = lfsaw KR (MCE [8, 7.23]) 0 * 3 + 80
 
 
-tg = out 0 $ tgrains 2 AR (impulse AR t 0) b 1 m d 0 0.1 2
-    where b = 10
-          m = mousex KR 0 (bufdur KR b) 0 0.2
-          t = mousey KR 2 200 1 0.2
-          d = 4 / t
-
-pg f = out 0 $ pan2 i l 1
-    where i = sinosc AR f 0 * envgen KR 1 1 0 1 2 envperc' * 0.1
-          l = mousex KR (-1) 1 0 0.1
-
-pt = out 0 $ pan2 i l 0.1
-    where i = sinosc AR 440 0
-          l = mousex KR (-1) 1 0 0.1
-
-ps = MRG [a, b]
-    where a = pauseself (mousex KR (-1) 1 0 0.1)
-          b = out 0 (sinosc AR 440 0 * 0.1)
-
-ts = Score [OscB 0.0 [d_recv' "test" ab],
-            OscB 1.0 [s_new "test" (-1) AddToTail 1],
-            OscB 3.0 [g_freeAll 1]]
