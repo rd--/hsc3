0be610a4c95c106569c3f63a67401503c535e0a9
Rohan Drape <rd@slavepianos.org>
2006-06-25 03:41:24 +0000

    Replace init' and stop' with reset'

--- Hsc/Play.hs
+++ Hsc/Play.hs
@@ -24,13 +24,12 @@ play fd u
     | hasOutputs u = playu fd (out (Constant 0) u)
     | otherwise    = playu fd u
 
-init_ fd = do send' fd (g_new 1 AddToTail 0)
-stop  fd = do send' fd (g_freeAll 1)
+reset fd = do send' fd (g_freeAll 0)
+              send' fd (g_new 1 AddToTail 0)
 
 withfd fd' a = do fd <- fd'
                   a fd
                   close' fd
 
-play' sc u = withfd sc (\fd -> play fd u)
-stop' sc   = withfd sc stop
-init' sc   = withfd sc init_
+play'  sc u = withfd sc (\fd -> play fd u)
+reset' sc   = withfd sc reset
