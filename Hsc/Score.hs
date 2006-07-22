309cbf6dbb9f973c8ff80e1e1b7a19cc3a689683
Rohan Drape <rd@slavepianos.org>
2006-07-22 10:09:35 +0000

    Implement playScore

--- Hsc/Score.hs
+++ Hsc/Score.hs
@@ -2,6 +2,8 @@ module Hsc.Score where
 
 import Hsc.OpenSoundControl
 import Hsc.U8v
+import Hsc.Schedule (utc, pauseUntil)
+import Hsc.Udp (send')
 
 data Score = Score [Osc]
              deriving (Eq, Show)
@@ -13,3 +15,11 @@ oscWithSize o = l ++ b
 writeScore fn (Score s) = u8vWrite fn b
     where b = concatMap oscWithSize s
 
+playBundle _  (OscM _ _) = error "playBundle: received Message"
+playBundle fd (OscB t l) = do pauseUntil t
+                              mapM (send' fd) l
+
+playScore fd (Score l) = do now <- utc
+                            mapM (playBundle fd) (map (f now) l)
+    where f now (OscB t l) = OscB (now + t) l
+          f _   (OscM _ _) = error "playScore: received Message"
