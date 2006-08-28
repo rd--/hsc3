cb1fe465b11ddc4f4ed81ee7aa677eab14d15c55
haskore <haskore@henning-thielemann.de>
2006-08-28 08:21:52 +0000

    make Rate instance of Enum class

--- Hsc/Rate.hs
+++ Hsc/Rate.hs
@@ -1,6 +1,6 @@
 module Hsc.Rate where
 
-data Rate = IR | KR | AR | DR deriving (Eq, Show)
+data Rate = IR | KR | AR | DR deriving (Eq, Show, Enum)
 
 instance Ord Rate where
     compare a b = compare (rateOrd a) (rateOrd b)
@@ -11,7 +11,4 @@ rateOrd KR = 2
 rateOrd AR = 3
 
 rateId :: Rate -> Int
-rateId IR = 0
-rateId KR = 1
-rateId AR = 2
-rateId DR = 3
+rateId = fromEnum
