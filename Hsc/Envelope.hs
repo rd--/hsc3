489fb9d18c6808bc0f072a7152bfc9fe60a45deb
Rohan Drape <rd@slavepianos.org>
2006-08-28 10:28:07 +0000

    Rename envLinen

--- Hsc/Envelope.hs
+++ Hsc/Envelope.hs
@@ -50,7 +50,7 @@ envSine :: UGen -> UGen -> [UGen]
 envSine dur lvl =
    env [0.0, lvl, 0.0] (dbl (dur / 2.0)) (dbl EnvSin) (-1.0) (-1.0)
 
-envLineN aT sT rT l c = env [0, l, l, 0] [aT, sT, rT] c (-1) (-1)
+envLinen aT sT rT l c = env [0, l, l, 0] [aT, sT, rT] c (-1) (-1)
 
 dbl :: a -> [a]
 dbl x = [x,x]
