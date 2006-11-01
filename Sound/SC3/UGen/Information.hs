68b331ec32bdd92847126d7498b9827073d0f410
Rohan Drape <rd@slavepianos.org>
2006-11-01 10:34:19 +0000

    Implement remaining Information UGens and order as in sclang

--- Sound/SC3/UGen/Information.hs
+++ Sound/SC3/UGen/Information.hs
@@ -1,22 +1,40 @@
 module Sound.SC3.UGen.Information where
 
-import Sound.SC3.UGen.Rate (Rate)
+import Sound.SC3.UGen.Rate (Rate(IR))
 import Sound.SC3.UGen.UGen (UGen, mkOsc)
 
-numAudioBuses    r = mkOsc r "NumAudioBuses"    [] 1 0
-numBuffers       r = mkOsc r "NumBuffers"       [] 1 0
-numControlBuses  r = mkOsc r "NumControlBuses"  [] 1 0
-numInputBuses    r = mkOsc r "NumInputBuses"    [] 1 0
-numOutputBuses   r = mkOsc r "NumOutputBuses"   [] 1 0
-numRunningSynths r = mkOsc r "NumRunningSynths" [] 1 0
-sampleDur        r = mkOsc r "SampleDur"        [] 1 0
-sampleRate       r = mkOsc r "SampleRate"       [] 1 0
+mkInfoUGen :: String -> UGen
+mkInfoUGen name = mkOsc IR name [] 1 0
 
-numAudioBuses :: Rate -> UGen
-numBuffers :: Rate -> UGen
-numControlBuses :: Rate -> UGen
-numInputBuses :: Rate -> UGen
-numOutputBuses :: Rate -> UGen
-numRunningSynths :: Rate -> UGen
-sampleDur :: Rate -> UGen
-sampleRate :: Rate -> UGen
+sampleRate :: UGen
+sampleRate = mkInfoUGen "SampleRate"
+
+sampleDur :: UGen
+sampleDur = mkInfoUGen "SampleDur"
+
+radiansPerSample :: UGen
+radiansPerSample = mkInfoUGen "RadiansPerSample"
+
+controlRate :: UGen
+controlRate = mkInfoUGen "ControlRate"
+
+subsampleOffset :: UGen
+subsampleOffset = mkInfoUGen "SubsampleOffset"
+
+numOutputBuses :: UGen
+numOutputBuses = mkInfoUGen "NumOutputBuses"
+
+numInputBuses :: UGen
+numInputBuses = mkInfoUGen "NumInputBuses"
+
+numAudioBuses :: UGen
+numAudioBuses = mkInfoUGen "NumAudioBuses"
+
+numControlBuses :: UGen
+numControlBuses = mkInfoUGen "NumControlBuses"
+
+numBuffers :: UGen
+numBuffers = mkInfoUGen "NumBuffers"
+
+numRunningSynths :: UGen
+numRunningSynths = mkInfoUGen "NumRunningSynths"
