3c2313cfeeaa3cc3fcf7defec9de36ebe8c3cf97
Rohan Drape <rd@slavepianos.org>
2006-11-12 06:51:05 +0000

    Add exports and haddock comment to SC3.Server.Schedule

--- Sound/SC3/Server/Schedule.hs
+++ Sound/SC3/Server/Schedule.hs
@@ -1,4 +1,4 @@
-module Sound.SC3.Server.Schedule where
+module Sound.SC3.Server.Schedule (utc, ntp, utc_ntp, pause, at) where
 
 import Control.Concurrent (threadDelay)
 import System.Time (ClockTime(TOD), getClockTime)
@@ -10,34 +10,40 @@ dti = floor
 itd :: Integer -> Double
 itd = fromIntegral
 
-utc :: IO Double
-utc = do TOD s p <- getClockTime
-         return (itd s + itd p / 1e12)
-
 secdif :: Integer
 secdif = (70 * 365 + 17) * 24 * 60 * 60
 
 secntp :: Double -> Integer
 secntp i = round (i * itd (2^(32::Int)))
 
+-- | Convert UTC timestamp `t' to NTP timestamp.
 utc_ntp :: Double -> Integer
-utc_ntp n = secntp (n + itd secdif)
+utc_ntp t = secntp (t + itd secdif)
 
+-- | Read current UTC timestamp.
+utc :: IO Double
+utc = do TOD s p <- getClockTime
+         return (itd s + itd p / 1e12)
+
+-- | Read current NTP timestamp.
 ntp :: IO Integer
 ntp = liftM utc_ntp utc
 
+-- | Pause for `n' seconds.
 pause :: Double -> IO ()
 pause n = when (n>0) (threadDelay (dti (n * 1e6)))
 
+-- | Pause until UTC time `t'. 
 pauseUntil :: Double -> IO ()
 pauseUntil t = do n <- utc
                   pause (t - n)
 
-at :: Double -> (Double -> IO Double) -> IO t
-at t f = do n <- f t
-            pauseUntil (t + n)
-            at (t + n) f
+at' :: Double -> (Double -> IO Double) -> IO t
+at' t f = do n <- f t
+             pauseUntil (t + n)
+             at' (t + n) f
 
-at' :: (Double -> IO Double) -> IO t
-at' f = do t <- utc
-           at t f
+-- | Pause until UTC time `t', apply `f' to `t', reschedule `f' at returned delta.
+at :: Double -> (Double -> IO Double) -> IO t
+at t f = do pauseUntil t
+            at' t f
