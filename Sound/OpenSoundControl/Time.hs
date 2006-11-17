00f7024264f69dcab294dd67fc5bf1dd5411484f
Rohan Drape <rd@slavepianos.org>
2006-11-17 12:35:34 +0000

    Simplify implementation of Sound/OpenSoundControl/Time.hs

--- Sound/OpenSoundControl/Time.hs
+++ Sound/OpenSoundControl/Time.hs
@@ -3,21 +3,20 @@ module Sound.OpenSoundControl.Time (utc, ntp, utc_ntp) where
 import System.Time (ClockTime(TOD), getClockTime)
 import Control.Monad (liftM)
 
-secdif :: Double
-secdif = (70 * 365 + 17) * 24 * 60 * 60
-
-secntp :: Double -> Integer
-secntp i = round (i * 4294967296.0)
+type UTC = Double
+type NTP = Integer
 
 -- | Convert UTC timestamp to NTP timestamp.
-utc_ntp :: Double {-^ UTC timestamp -} -> Integer
+utc_ntp :: UTC -> NTP
 utc_ntp t = secntp (t + secdif)
+    where secntp i = round (i * 4294967296.0)
+          secdif = (70 * 365 + 17) * 24 * 60 * 60
 
 -- | Read current UTC timestamp.
-utc :: IO Double
+utc :: IO UTC
 utc = do TOD s p <- getClockTime
          return (fromIntegral s + fromIntegral p / 1e12)
 
 -- | Read current NTP timestamp.
-ntp :: IO Integer
+ntp :: IO NTP
 ntp = liftM utc_ntp utc
