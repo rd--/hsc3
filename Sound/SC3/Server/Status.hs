e3bbe6da9b3e6ffd4d8c2dec26e9188b97012d1f
Rohan Drape <rd@slavepianos.org>
2007-03-04 00:29:42 +0000

    Shift SC3.Server OSC communication from UDP to TCP

--- Sound/SC3/Server/Status.hs
+++ Sound/SC3/Server/Status.hs
@@ -2,7 +2,7 @@ module Sound.SC3.Server.Status (serverStatus) where
 
 import Sound.OpenSoundControl (OSC(Message))
 import Sound.SC3.Server.Command (status)
-import Sound.OpenSoundControl.UDP (UDP, send, wait)
+import Sound.OpenSoundControl.TCP (TCP, send, wait)
 
 statusFields :: [String]
 statusFields = ["# UGens                     ", 
@@ -23,7 +23,7 @@ statusFormat r = s : zipWith (++) statusFields (statusInfo r)
     where s = "***** SuperCollider Server Status *****"
 
 -- | Collect server status information.
-serverStatus :: UDP -> IO [String]
+serverStatus :: TCP -> IO [String]
 serverStatus fd = do send fd status 
                      r <- wait fd "status.reply"
                      return (statusFormat r)
