9dbdc9e773cb927281e98d119a871b14506b70e5
Rohan Drape <rd@slavepianos.org>
2007-03-04 03:19:12 +0000

    Accomodate edits hosc that introduce a 'Transport' type

--- Sound/SC3/Server/Play.hs
+++ Sound/SC3/Server/Play.hs
@@ -1,8 +1,6 @@
 module Sound.SC3.Server.Play (play, stop, reset, withSC3, audition) where
 
-import Sound.OpenSoundControl.OSC (OSC)
---import Sound.OpenSoundControl.UDP (UDP, udp, send, wait, withUDP)
-import Sound.OpenSoundControl.TCP (TCP, tcp, send, wait, withTCP)
+import Sound.OpenSoundControl
 import Sound.SC3.UGen.UGen (UGen(..))
 import Sound.SC3.UGen.IO (addOut)
 import Sound.SC3.UGen.Graph (graph)
@@ -10,7 +8,7 @@ import Sound.SC3.Server.Graphdef (graphdef)
 import Sound.SC3.Server.Command (AddAction(AddToTail), s_new, d_recv, g_new, g_freeAll)
 
 -- | Construct an instrument definition, send /d_recv and /s_new messages to scsynth.
-play :: UGen -> TCP -> IO OSC
+play :: UGen -> Transport -> IO OSC
 play u fd = do let g = graphdef "Anonymous" (graph (addOut u))
                send fd (d_recv g) 
                r <- wait fd "/done"
@@ -18,17 +16,17 @@ play u fd = do let g = graphdef "Anonymous" (graph (addOut u))
                return r
 
 -- | Free all nodes at the group with node id 1.
-stop :: TCP -> IO ()
+stop :: Transport -> IO ()
 stop fd = send fd (g_freeAll [1])
 
 -- | Free all nodes and re-create group node with id 1.
-reset :: TCP -> IO ()
+reset :: Transport -> IO ()
 reset fd = do send fd (g_freeAll [0])
               send fd (g_new [(1, AddToTail, 0)])
 
 -- | Bracket SC3 communication.
-withSC3 :: (TCP -> IO a) -> IO a
-withSC3 = withTCP (tcp "127.0.0.1" 57110)
+withSC3 :: (Transport -> IO a) -> IO a
+withSC3 = withTransport (tcp "127.0.0.1" 57110)
 
 -- | withSC3 . play
 audition :: UGen -> IO OSC
--- Sound/SC3/Server/Status.hs
+++ Sound/SC3/Server/Status.hs
@@ -1,8 +1,7 @@
 module Sound.SC3.Server.Status (serverStatus) where
 
-import Sound.OpenSoundControl (OSC(Message))
+import Sound.OpenSoundControl
 import Sound.SC3.Server.Command (status)
-import Sound.OpenSoundControl.TCP (TCP, send, wait)
 
 statusFields :: [String]
 statusFields = ["# UGens                     ", 
@@ -23,7 +22,7 @@ statusFormat r = s : zipWith (++) statusFields (statusInfo r)
     where s = "***** SuperCollider Server Status *****"
 
 -- | Collect server status information.
-serverStatus :: TCP -> IO [String]
+serverStatus :: Transport -> IO [String]
 serverStatus fd = do send fd status 
                      r <- wait fd "status.reply"
                      return (statusFormat r)
