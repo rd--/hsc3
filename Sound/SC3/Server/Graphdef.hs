15e1a815741059a518f3c2e46d9565f67aaadb03
Rohan Drape <rd@slavepianos.org>
2007-03-04 00:28:21 +0000

    Shift Graphdef constructor from OSC.U8v to OSC.Byte encoder

--- Sound/SC3/Server/Graphdef.hs
+++ Sound/SC3/Server/Graphdef.hs
@@ -1,25 +1,30 @@
 module Sound.SC3.Server.Graphdef (graphdef) where
 
-import Sound.OpenSoundControl
+import Sound.OpenSoundControl.Byte
+import Sound.OpenSoundControl.Cast
 import Sound.SC3.UGen.UGen (UGen(..))
 import Sound.SC3.UGen.Rate (rateId)
 import Sound.SC3.UGen.Graph
 
+import Data.Word
+import qualified Data.ByteString.Lazy as B
+
 -- | Byte-encode Input value.
-input_u8v :: Input -> [U8]
-input_u8v (Input u p) = i16_u8v u ++ i16_u8v p
+encode_input :: Input -> B.ByteString
+encode_input (Input u p) = B.append (encode_i16 u) (encode_i16 p)
 
 -- | Byte-encode UGen value.
-ugen_u8v :: Graph -> UGen -> [U8]
-ugen_u8v g c@(Control _ n _)    = str_pstr n ++ i16_u8v (controlIndex g c)
-ugen_u8v g (UGen r n i o s _)   = str_pstr n ++
-                                  i8_u8v (rateId r) ++
-                                  i16_u8v (length i) ++
-                                  i16_u8v (length o) ++
-                                  i16_u8v s ++
-                                  concatMap (input_u8v . makeInput g) i ++
-                                  concatMap (i8_u8v . rateId) o
-ugen_u8v _ _                    = error "illegal input"
+encode_ugen :: Graph -> UGen -> B.ByteString
+encode_ugen g c@(Control _ n _) = B.concat [B.pack (str_pstr n),
+                                            encode_i16 (controlIndex g c)]
+encode_ugen g (UGen r n i o s _) = B.concat [B.pack (str_pstr n),
+                                             encode_i8 (rateId r),
+                                             encode_i16 (length i),
+                                             encode_i16 (length o),
+                                             encode_i16 s,
+                                             B.concat (map (encode_input . makeInput g) i),
+                                             B.concat (map (encode_i8 . rateId) o)]
+encode_ugen _ _  = error "illegal input"
 
 -- | Value of Constant.
 constantValue :: UGen -> Double
@@ -32,16 +37,21 @@ controlDefault (Control _ _ n) = n
 controlDefault  _              = error "controlDefault: non Control input"
 
 -- | Construct instrument definition bytecode.
-graphdef :: String -> Graph -> [U8]
-graphdef s g@(Graph n c u) = str_u8v "SCgf" ++
-                             i32_u8v 0 ++
-                             i16_u8v 1 ++
-                             str_pstr s ++
-                             i16_u8v (length n) ++
-                             concatMap (f32_u8v . f64_f32 . constantValue) n ++
-                             i16_u8v (length c) ++
-                             concatMap (f32_u8v . f64_f32 . controlDefault) c ++
-                             i16_u8v (length c) ++
-                             concatMap (ugen_u8v g) c ++
-                             i16_u8v (length u) ++
-                             concatMap (ugen_u8v g) u
+encode_graphdef :: String -> Graph -> B.ByteString
+encode_graphdef s g@(Graph n c u) = B.concat [
+                                     encode_str "SCgf",
+                                     encode_i32 0,
+                                     encode_i16 1,
+                                     B.pack (str_pstr s),
+                                     encode_i16 (length n),
+                                     B.concat (map (encode_f32 . constantValue) n),
+                                     encode_i16 (length c),
+                                     B.concat (map (encode_f32 . controlDefault) c),
+                                     encode_i16 (length c),
+                                     B.concat (map (encode_ugen g) c),
+                                     encode_i16 (length u),
+                                     B.concat (map (encode_ugen g) u)]
+
+-- | Construct instrument definition bytecode.
+graphdef :: String -> Graph -> [Word8]
+graphdef s g = B.unpack (encode_graphdef s g)
