b0d7f2d833840db9fe37143f537830bdc3bad9e6
Rohan Drape <rd@slavepianos.org>
2007-03-29 12:31:33 +0000

    Haddock documentation for Information UGens

--- Sound/SC3/UGen/Information.hs
+++ Sound/SC3/UGen/Information.hs
@@ -6,35 +6,46 @@ import Sound.SC3.UGen.UGen (UGen, mkOsc)
 mkInfoUGen :: String -> UGen
 mkInfoUGen name = mkOsc IR name [] 1 0
 
+-- | Sample rate of synthesis server, frames per second.
 sampleRate :: UGen
 sampleRate = mkInfoUGen "SampleRate"
 
+-- | Duration of one sample, seconds.
 sampleDur :: UGen
 sampleDur = mkInfoUGen "SampleDur"
 
+-- | Duration of one sample, radians.
 radiansPerSample :: UGen
 radiansPerSample = mkInfoUGen "RadiansPerSample"
 
+-- | Control rate of synthesis server, periods per second.
 controlRate :: UGen
 controlRate = mkInfoUGen "ControlRate"
 
+-- | Sub-sample accurate scheduling offset.
 subsampleOffset :: UGen
 subsampleOffset = mkInfoUGen "SubsampleOffset"
 
+-- | Number of allocated output audio rate buses.
 numOutputBuses :: UGen
 numOutputBuses = mkInfoUGen "NumOutputBuses"
 
+-- | Number of allocated input audio rate buses.
 numInputBuses :: UGen
 numInputBuses = mkInfoUGen "NumInputBuses"
 
+-- | Number of allocated audio rate buses.
 numAudioBuses :: UGen
 numAudioBuses = mkInfoUGen "NumAudioBuses"
 
+-- | Number of allocated control rate buses.
 numControlBuses :: UGen
 numControlBuses = mkInfoUGen "NumControlBuses"
 
+-- | Number of allocated buffers.
 numBuffers :: UGen
 numBuffers = mkInfoUGen "NumBuffers"
 
+-- | Number of runnings synthesis nodes.
 numRunningSynths :: UGen
 numRunningSynths = mkInfoUGen "NumRunningSynths"
