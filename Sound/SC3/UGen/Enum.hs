b47ec1d169a1db8ebe9265578c1e6b613aed158c
Rohan Drape <rd@slavepianos.org>
2007-07-01 04:31:51 +0000

    Add haddock comments for UGen.Enum types and move adaptors to SC3.UGen.Private module

--- Sound/SC3/UGen/Enum.hs
+++ Sound/SC3/UGen/Enum.hs
@@ -2,47 +2,39 @@ module Sound.SC3.UGen.Enum where
 
 import Sound.SC3.UGen.UGen (UGen(..))
 
+-- | Loop indicator input.
 data Loop = Loop
           | NoLoop
           | WithLoop UGen
             deriving (Eq, Show)
 
-fromLoop :: Loop -> UGen
-fromLoop NoLoop       = Constant 0
-fromLoop Loop         = Constant 1
-fromLoop (WithLoop u) = u
-
+-- | Interpolation indicator input.
 data Interpolation = NoInterpolation
                    | LinearInterpolation
                    | CubicInterpolation
                    | Interpolation UGen
                      deriving (Eq, Show)
 
-fromInterpolation :: Interpolation -> UGen
-fromInterpolation NoInterpolation     = Constant 1
-fromInterpolation LinearInterpolation = Constant 2
-fromInterpolation CubicInterpolation  = Constant 4
-fromInterpolation (Interpolation u)   = u
-
+-- | Completion mode indicator input.
 data DoneAction = DoNothing
                 | PauseSynth
                 | RemoveSynth
                 | DoneAction UGen
                   deriving (Eq, Show)
 
-fromDoneAction :: DoneAction -> UGen
-fromDoneAction DoNothing      = Constant 0
-fromDoneAction PauseSynth     = Constant 1
-fromDoneAction RemoveSynth    = Constant 2
-fromDoneAction (DoneAction u) = u
-
+-- | Warp interpolation indicator input.
 data Warp = Linear
           | Exponential
           | Warp UGen
             deriving (Eq, Show)
 
-fromWarp :: Warp -> UGen
-fromWarp Linear      = Constant 0
-fromWarp Exponential = Constant 1
-fromWarp (Warp u)    = u
-
+-- | Envelope curve indicator input.
+data EnvCurve = EnvStep
+              | EnvLin
+              | EnvExp
+              | EnvSin
+              | EnvCos
+              | EnvNum UGen
+              | EnvSqr
+              | EnvCub
+              deriving (Eq, Show)
---
+++ Sound/SC3/UGen/Private.hs
@@ -0,0 +1,53 @@
+module Sound.SC3.UGen.Private where
+
+import Sound.SC3.UGen.Enum
+import Sound.SC3.UGen.UGen (UGen(..))
+
+fromLoop :: Loop -> UGen
+fromLoop NoLoop       = Constant 0
+fromLoop Loop         = Constant 1
+fromLoop (WithLoop u) = u
+
+fromInterpolation :: Interpolation -> UGen
+fromInterpolation NoInterpolation     = Constant 1
+fromInterpolation LinearInterpolation = Constant 2
+fromInterpolation CubicInterpolation  = Constant 4
+fromInterpolation (Interpolation u)   = u
+
+fromDoneAction :: DoneAction -> UGen
+fromDoneAction DoNothing      = Constant 0
+fromDoneAction PauseSynth     = Constant 1
+fromDoneAction RemoveSynth    = Constant 2
+fromDoneAction (DoneAction u) = u
+
+fromWarp :: Warp -> UGen
+fromWarp Linear      = Constant 0
+fromWarp Exponential = Constant 1
+fromWarp (Warp u)    = u
+
+env_curve :: EnvCurve -> UGen
+env_curve EnvStep    = 0.0
+env_curve EnvLin     = 1.0
+env_curve EnvExp     = 2.0
+env_curve EnvSin     = 3.0
+env_curve EnvCos     = 4.0
+env_curve (EnvNum _) = 5.0
+env_curve EnvSqr     = 6.0
+env_curve EnvCub     = 7.0
+
+env_value :: EnvCurve -> UGen
+env_value (EnvNum u) = u
+env_value _          = 0.0
+
+d_dx :: (Num a) => [a] -> [a]
+d_dx [] = []
+d_dx [_] = []
+d_dx [x,y] = [y - x]
+d_dx (x:y:r) = y - x : d_dx (y:r)
+
+dbl :: a -> [a]
+dbl x = [x,x]
+
+mkInfoUGen :: String -> UGen
+mkInfoUGen name = mkOsc IR name [] 1 0
+
