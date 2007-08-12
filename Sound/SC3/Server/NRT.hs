4e33f1239717ea116793f10f048db9fa12ff12e4
Rohan Drape <rd@slavepianos.org>
2007-08-12 02:34:31 +0000

    Simplify encodeNRT, use encodeOSC_NTP from hosc

--- Sound/SC3/Server/NRT.hs
+++ Sound/SC3/Server/NRT.hs
@@ -6,18 +6,12 @@ import qualified Data.ByteString.Lazy as B
 
 oscWithSize :: OSC -> B.ByteString
 oscWithSize o = B.append l b
-    where b = encodeOSC o
+    where b = encodeOSC_NTP o
           l = encode_i32 (fromIntegral (B.length b))
-
--- (ntp->utc. 0) ==> -2208988800
-ntpZeroed :: [OSC] -> [OSC]
-ntpZeroed l = map f l
-    where f (Bundle t c) = Bundle (t - 2208988800.0) c
-          f (Message _ _) = undefined
 
 -- Encode a list of OSC bundles as an NRT score.
 encodeNRT :: [OSC] -> B.ByteString
-encodeNRT s = B.concat (map oscWithSize (ntpZeroed s))
+encodeNRT s = B.concat (map oscWithSize s)
 
 -- Write an list of OSC bundles as an NRT score.
 writeNRT :: FilePath -> [OSC] -> IO ()
