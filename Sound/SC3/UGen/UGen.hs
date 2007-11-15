1688562fdd13f28d880ec45a01f5204932ec638a
Rohan Drape <rd@slavepianos.org>
2007-11-15 22:14:53 +0000

    Whitespace edits (type declaration alignments)

--- Sound/SC3/UGen/UGen.hs
+++ Sound/SC3/UGen/UGen.hs
@@ -5,25 +5,25 @@ import Control.Monad (liftM, replicateM)
 import Sound.SC3.UGen.Rate (Rate)
 import Sound.SC3.UGen.UId
 
-type Name    = String
-type Output  = Rate
+type Name = String
+type Output = Rate
 newtype Special = Special Int deriving (Eq, Show)
 newtype UGenId = UGenId Int deriving (Eq, Show)
-data UGen    = Constant { constantValue :: Double }
-             | Control { controlRate_ :: Rate
-                       , controlName :: Name
-                       , controlDefault :: Double }
-             | UGen { ugenRate :: Rate
-                    , ugenName :: Name
-                    , ugenInputs :: [UGen]
-                    , ugenOuputs :: [Output]
-                    , ugenSpecial :: Special
-                    , ugenId :: UGenId }
-             | Proxy { proxySource :: UGen
-                     , proxyIndex :: Int }
-             | MCE { mceProxies :: [UGen] }
-             | MRG { mrgRoots :: [UGen] }
-               deriving (Eq, Show)
+data UGen = Constant { constantValue :: Double }
+          | Control { controlRate_ :: Rate
+                    , controlName :: Name
+                    , controlDefault :: Double }
+          | UGen { ugenRate :: Rate
+                 , ugenName :: Name
+                 , ugenInputs :: [UGen]
+                 , ugenOuputs :: [Output]
+                 , ugenSpecial :: Special
+                 , ugenId :: UGenId }
+          | Proxy { proxySource :: UGen
+                  , proxyIndex :: Int }
+          | MCE { mceProxies :: [UGen] }
+          | MRG { mrgRoots :: [UGen] }
+            deriving (Eq, Show)
 
 -- | Clone UGen.
 clone :: (UId m) => Int -> m UGen -> m UGen
