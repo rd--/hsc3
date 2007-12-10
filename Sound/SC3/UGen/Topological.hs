81db610e1cb84e10f75bc3cf4a1c57e36aa89c0d
Rohan Drape <rd@slavepianos.org>
2007-12-10 02:22:01 +0000

    Initial topological sort for UGen graphs
    http://create.ucsb.edu/pipermail/sc-users/2003-March/002807.html
    
    This simple graph can test the topological sort
    of the graphdef, it ought only to use a minimal
    number of interconnect buffers (the below works
    with 'scsynth -u 57110 -w 4').
    
    > let n = 122
    >     c = envCoord [(0,0), (0.15, 1), (6, 0)] 1 1 EnvLin
    >     e = envGen KR 1 1 0 1 RemoveSynth c
    >     s = mixFill n (\n -> sinOsc AR (440 + fromIntegral n) 0 * 0.001)
    > audition (out 0 (s * e))

---
+++ Sound/SC3/UGen/Topological.hs
@@ -0,0 +1,21 @@
+module Sound.SC3.UGen.Topological (tsort) where
+
+import Data.List
+import Sound.SC3.UGen.UGen
+import Sound.SC3.UGen.UGen.Predicate
+
+-- Topological sort.
+
+type D = (Int, UGen)
+
+setDepth :: Int -> [UGen] -> [D] -> [D]
+setDepth d u l = map f l
+    where f (d', u') = if u' `elem` u then (max d d', u') else (d', u')
+
+rsort :: Int -> [UGen] -> [D] -> [D]
+rsort d r l = if null i then l else rsort (d + 1) i (setDepth d i l)
+    where i = concatMap (filter isUGen . ugenInputs) r
+
+tsort :: UGen -> [UGen] -> [UGen]
+tsort r u = reverse (map snd (sortBy cmp (rsort 1 [r] (map ((,) 0) u))))
+    where cmp (d1,_) (d2,_) = compare d1 d2
