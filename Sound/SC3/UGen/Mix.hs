8500d4cd7b4f8d61e1aa737c61eafe9d03cdc56c
Rohan Drape <rd@slavepianos.org>
2007-12-14 05:55:56 +0000

    Implement freqShift

--- Sound/SC3/UGen/Mix.hs
+++ Sound/SC3/UGen/Mix.hs
@@ -1,5 +1,8 @@
-module Sound.SC3.UGen.Mix (mix, mixFill) where
+module Sound.SC3.UGen.Mix (mix, mixFill, freqShift) where
 
+import Sound.SC3.UGen.Filter
+import Sound.SC3.UGen.Oscillator
+import Sound.SC3.UGen.Rate
 import Sound.SC3.UGen.UGen
 import Sound.SC3.UGen.UGen.Math ()
 
@@ -11,3 +14,9 @@ mix u        = u
 -- | Construct and sum a set of UGens.
 mixFill :: Int -> (Int -> UGen) -> UGen
 mixFill n f = mix (MCE (map f [0..n-1]))
+
+-- | Frequency shifter, in terms of Hilbert UGen.
+freqShift :: UGen -> UGen -> UGen -> UGen
+freqShift i f p = mix (h * o)
+    where o = sinOsc AR f (MCE [p + 0.5 * pi, p])
+          h = hilbert i
