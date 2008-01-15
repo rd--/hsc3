12a53e60c79e545296b2c2a9761b5e7ef92af8c0
Rohan Drape <rd@slavepianos.org>
2008-01-15 14:28:27 +0000

    Implement auditionG

--- Sound/SC3/Server/Play.hs
+++ Sound/SC3/Server/Play.hs
@@ -1,18 +1,23 @@
-module Sound.SC3.Server.Play (play, stop, reset, withSC3, audition) where
+module Sound.SC3.Server.Play ( play, stop, reset
+                             , withSC3, audition, auditionG ) where
 
 import Sound.OpenSoundControl
-import Sound.SC3.UGen.UGen (UGen(..))
-import Sound.SC3.UGen.Graph (graph)
-import Sound.SC3.Server.Graphdef (graphdef)
-import Sound.SC3.Server.Command (AddAction(AddToTail), s_new, d_recv, g_new, g_freeAll)
+import Sound.SC3.UGen.UGen
+import Sound.SC3.UGen.Graph
+import Sound.SC3.Server.Graphdef
+import Sound.SC3.Server.Command
 
--- | Construct an instrument definition, send /d_recv and /s_new messages to scsynth.
+playG :: Transport t => t -> Graph -> IO OSC
+playG fd g = do let d = graphdef "Anonymous" g
+                send fd (d_recv d) 
+                r <- wait fd "/done"
+                send fd (s_new "Anonymous" (-1) AddToTail 1 [])
+                return r
+
+-- | Construct an instrument definition, send /d_recv and /s_new
+-- | messages to scsynth.
 play :: Transport t => t -> UGen -> IO OSC
-play fd u = do let g = graphdef "Anonymous" (graph u)
-               send fd (d_recv g) 
-               r <- wait fd "/done"
-               send fd (s_new "Anonymous" (-1) AddToTail 1 [])
-               return r
+play fd = playG fd . graph
 
 -- | Free all nodes at the group with node id 1.
 stop :: Transport t => t -> IO ()
@@ -27,6 +32,9 @@ reset fd = do send fd (g_freeAll [0])
 withSC3 :: (UDP -> IO a) -> IO a
 withSC3 = withTransport (openUDP "127.0.0.1" 57110)
 
+auditionG :: Graph -> IO ()
+auditionG g = withSC3 (\fd -> playG fd g) >> return ()
+
 -- | withSC3 . play
 audition :: UGen -> IO ()
-audition g = withSC3 (\fd -> play fd g) >> return ()
+audition = auditionG . graph
