2754214b28104c31cfd6f34b2e7de354c8a261b4
Rohan Drape <rd@slavepianos.org>
2008-01-27 09:54:20 +0000

    Implement Sound.SC3.UGen.Base module

---
+++ Sound/SC3/UGen/Base.hs
@@ -0,0 +1,7 @@
+module Sound.SC3.UGen.Base (module Sound.SC3.UGen.Demand.Base,
+                            module Sound.SC3.UGen.FFT.Base,
+                            module Sound.SC3.UGen.Noise.Base) where
+
+import Sound.SC3.UGen.Demand.Base
+import Sound.SC3.UGen.FFT.Base
+import Sound.SC3.UGen.Noise.Base
