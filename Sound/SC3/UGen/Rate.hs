3c85d87d43a1596f764d32535953984f2f42d51b
Rohan Drape <rd@slavepianos.org>
2008-03-26 01:43:19 +0000

    Add ar=AR etc.
    Not sure of this, aesthetic only

--- Sound/SC3/UGen/Rate.hs
+++ Sound/SC3/UGen/Rate.hs
@@ -1,10 +1,18 @@
-module Sound.SC3.UGen.Rate (Rate(..), rateId) where
+module Sound.SC3.UGen.Rate ( Rate(..)
+                           , rateId
+                           , ar, kr, ir, dr ) where
 
 -- | Operating rate of UGen.
 data Rate = IR | KR | AR | DR deriving (Eq, Show, Enum)
 
 instance Ord Rate where
     compare a b = compare (rateOrd a) (rateOrd b)
+
+ar, kr, ir, dr :: Rate
+ar = AR
+kr = KR
+ir = IR
+dr = DR
 
 rateOrd :: Rate -> Int
 rateOrd IR = 0
