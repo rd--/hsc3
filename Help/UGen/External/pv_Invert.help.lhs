17e68f83784f21d5a973db94410686875b77340e
Rohan Drape <rd@slavepianos.org>
2008-04-04 00:52:22 +0000

    binding and help file for pv_Invert

---
+++ Help/UGen/External/pv_Invert.help.lhs
@@ -0,0 +1,9 @@
+pv_Invert buffer
+
+> let { s = sinOsc AR 440 0 * 0.4
+>     ; n = Sound.SC3.UGen.Base.pinkNoise (uid 0) AR * 0.1
+>     ; i = s + n
+>     ; c0 = fft' 10 i
+>     ; c1 = pv_Invert c0 }
+> in withSC3 (\fd -> do { send fd (b_alloc 10 2048 1)
+>                       ; audition (out 0 (mce2 i (ifft' c1) * 0.5)) })
--- Sound/SC3/UGen/External.hs
+++ Sound/SC3/UGen/External.hs
@@ -4,10 +4,8 @@ import Sound.SC3.UGen.Rate
 import Sound.SC3.UGen.UGen
 import Sound.SC3.UGen.UGen.Construct
 
-ay :: UGen -> UGen -> UGen -> UGen -> UGen -> UGen 
-   -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen
-ay ta tb tc n c va vb vc ef es ct = 
-    mkOsc AR "AY" [ta, tb, tc, n, c, va, vb, vc, ef, es, ct] 1
+ay :: UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen -> UGen
+ay ta tb tc n c va vb vc ef es ct = mkOsc AR "AY" [ta, tb, tc, n, c, va, vb, vc, ef, es, ct] 1
 
 ayFreqToTone :: Fractional a => a -> a
 ayFreqToTone f = 110300 / (f - 0.5)
@@ -17,3 +15,10 @@ membraneCircle i t l = mkOsc AR "MembraneCircle" [i, t, l] 1
 
 membraneHexagon :: UGen -> UGen -> UGen -> UGen
 membraneHexagon i t l = mkOsc AR "MembraneHexagon" [i, t, l] 1
+
+pv_Invert :: UGen -> UGen
+pv_Invert b = mkOsc KR "PV_Invert" [b] 1
+
+-- Local Variables:
+-- truncate-lines:t
+-- End:
