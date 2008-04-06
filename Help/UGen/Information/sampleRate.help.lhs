14b1234f041f2b3cb567b072f4ea0234d77434da
Rohan Drape <rd@slavepianos.org>
2008-04-06 02:20:30 +0000

    implement serverSampleRate[Actual|Nominal]

--- Help/UGen/Information/sampleRate.help.lhs
+++ Help/UGen/Information/sampleRate.help.lhs
@@ -6,3 +6,10 @@ Compare a sine tone derived from sample rate with a 440Hz tone.
 
 > let f = mce [sampleRate * 0.01, 440]
 > in audition (out 0 (sinOsc AR f 0 * 0.1))
+
+The server status command can extract nominal and
+actual sample rates from a running server.
+
+> withSC3 (\fd -> liftM2 (,)
+>                        (serverSampleRateNominal fd) 
+>                        (serverSampleRateActual fd))
--- Sound/SC3/Server/Status.hs
+++ Sound/SC3/Server/Status.hs
@@ -1,16 +1,36 @@
-module Sound.SC3.Server.Status (serverStatus) where
+module Sound.SC3.Server.Status ( serverStatus
+                               , serverSampleRateNominal
+                               , serverSampleRateActual ) where
 
+import Control.Monad
 import Sound.OpenSoundControl
 import Sound.SC3.Server.Command
 
 -- | Collect server status information.
 serverStatus :: Transport t => t -> IO [String]
-serverStatus fd = do send fd status 
-                     r <- wait fd "status.reply"
-                     return (statusFormat r)
+serverStatus fd = liftM statusFormat (serverStatusData fd)
+
+-- | Read nominal sample rate of server.
+serverSampleRateNominal :: (Transport t) => t -> IO Double
+serverSampleRateNominal fd = liftM (extractDouble . (!! 7)) (serverStatusData fd)
+
+-- | Read actual sample rate of server.
+serverSampleRateActual :: (Transport t) => t -> IO Double
+serverSampleRateActual fd = liftM (extractDouble . (!! 8)) (serverStatusData fd)
+
+extractDouble :: Datum -> Double
+extractDouble (Float f) = f
+extractDouble (Double f) = f
+extractDouble _ = error "extractDouble"
+
+serverStatusData :: Transport t => t -> IO [Datum]
+serverStatusData fd = do send fd status 
+                         (Message _ d) <- wait fd "status.reply"
+                         return d
 
 statusFields :: [String]
-statusFields = ["# UGens                     ", 
+statusFields = ["Unused                      ",
+                "# UGens                     ", 
                 "# Synths                    ", 
                 "# Groups                    ",
                 "# Instruments               ",
@@ -19,12 +39,8 @@ statusFields = ["# UGens                     ",
                 "Sample Rate (Nominal)       ",
                 "Sample Rate (Actual)        "]
 
-statusInfo :: OSC -> [String]
-statusInfo o = maybe [] f (address o)
-    where f a = if a == "status.reply" 
-                then maybe [] (map show . tail) (arguments o) 
-                else []
-
-statusFormat :: OSC -> [String]
-statusFormat r = s : zipWith (++) statusFields (statusInfo r)
+statusFormat :: [Datum] -> [String]
+statusFormat d = s : zipWith (++) 
+                             (tail statusFields) 
+                             (map show (tail d))
     where s = "***** SuperCollider Server Status *****"
