52616a0065f824cac567a9ca8cee953c7eee9d25
stefan kersten <sk@k-hornz.de>
2008-04-23 16:03:55 +0000

    Add putNRT for writing OSC packets to a file handle

--- Sound/SC3/Server/NRT.hs
+++ Sound/SC3/Server/NRT.hs
@@ -1,8 +1,10 @@
 module Sound.SC3.Server.NRT ( encodeNRT 
-                            , writeNRT ) where
+                            , writeNRT
+                            , putNRT ) where
 
 import qualified Data.ByteString.Lazy as B
 import Sound.OpenSoundControl
+import System.IO (Handle)
 
 -- | Encode and prefix with encoded length.
 oscWithSize :: OSC -> B.ByteString
@@ -17,3 +19,7 @@ encodeNRT s = B.concat (map oscWithSize s)
 -- | Write a list of OSC bundles as an NRT score.
 writeNRT :: FilePath -> [OSC] -> IO ()
 writeNRT fn s = B.writeFile fn (encodeNRT s)
+
+-- | Write a list of OSC bundles as an NRT score to a file handle.
+putNRT :: Handle -> [OSC] -> IO ()
+putNRT h s = B.hPut h (encodeNRT s)
