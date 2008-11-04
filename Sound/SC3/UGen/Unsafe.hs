2e44b06e7ccee3c1a28e8cd92e10bd3ca9f6c180
Rohan Drape <rd@slavepianos.org>
2008-11-04 08:47:26 +0000

    delete unsafe unit generator interface
    moved to separate hsc3-unsafe project

--- Sound/SC3/UGen/Unsafe.hs
+++
@@ -1,7 +0,0 @@
-module Sound.SC3.UGen.Unsafe (module Sound.SC3.UGen.Demand.Unsafe,
-                              module Sound.SC3.UGen.FFT.Unsafe,
-                              module Sound.SC3.UGen.Noise.Unsafe) where
-
-import Sound.SC3.UGen.Demand.Unsafe
-import Sound.SC3.UGen.FFT.Unsafe
-import Sound.SC3.UGen.Noise.Unsafe
