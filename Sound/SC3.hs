5f6437de547e11fa91a4401cfc20a9947336f9d4
Brent Yorgey <byorgey@cis.upenn.edu>
2009-03-17 01:02:32 +0000

    add some meta-documentation to Sound.SC3

--- Sound/SC3.hs
+++ Sound/SC3.hs
@@ -1,6 +1,24 @@
-module Sound.SC3 (module Sound.SC3.UGen,
+module Sound.SC3 (
+                  -- $help
+
+                  module Sound.SC3.UGen,
                   module Sound.SC3.Server) where
 
 import Sound.SC3.UGen
 import Sound.SC3.Server
+
+-- $help
+-- Once the hsc3 library has been installed, you will find help files
+-- installed in @$PREFIX\/share\/hsc3-VERSION\/Help\/@.
+--
+-- For installation and configuration information please consult the
+-- tutorial file at @Help\/Tutorial\/Tutorial.lhs@.
+--
+-- For general information on supercollider and the overall design of
+-- the hsc3 bindings, see @Help\/hsc3.help.lhs@.
+--
+-- For documentation and examples for unit generators, see
+-- @Help\/UGen\/@.  Note that the Haddock documentation for many unit
+-- generators is incomplete; see the appropriate file in
+-- @Help\/UGen\/@ for complete documentation.
 
