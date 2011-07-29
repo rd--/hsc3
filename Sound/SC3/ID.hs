fac9b7d27e59cc9437b89d156c0a1a980d668fda
Rohan Drape <rd@slavepianos.org>
2011-07-29 11:54:37 +0000

    UGen.Composite.ID

--- Sound/SC3/ID.hs
+++ Sound/SC3/ID.hs
@@ -1,5 +1,6 @@
 module Sound.SC3.ID (module Sound.SC3.Identifier
                     ,module Sound.SC3.UGen
+                    ,module Sound.SC3.UGen.Composite.ID
                     ,module Sound.SC3.UGen.Demand.ID
                     ,module Sound.SC3.UGen.FFT.ID
                     ,module Sound.SC3.UGen.Noise.ID
@@ -7,6 +8,7 @@ module Sound.SC3.ID (module Sound.SC3.Identifier
 
 import Sound.SC3.Identifier
 import Sound.SC3.UGen
+import Sound.SC3.UGen.Composite.ID
 import Sound.SC3.UGen.Demand.ID
 import Sound.SC3.UGen.FFT.ID
 import Sound.SC3.UGen.Noise.ID
--- Sound/SC3/UGen/Composite/Monadic.hs
+++ Sound/SC3/UGen/Composite/Monadic.hs
@@ -8,14 +8,17 @@ import Sound.SC3.UGen.UId
 
 -- | Demand rate (:) function.
 dcons :: (UId m) => UGen -> UGen -> m UGen
-dcons x xs = do i <- dseq 1 (mce2 0 1)
-                a <- dseq 1 (mce2 x xs)
-                dswitch i a
+dcons x xs = do
+  i <- dseq 1 (mce2 0 1)
+  a <- dseq 1 (mce2 x xs)
+  dswitch i a
 
 tChoose :: (UId m) => UGen -> UGen -> m UGen
-tChoose t a = do r <- tiRand 0 (constant (length (mceChannels a))) t
-                 return (select r a)
+tChoose t a = do
+  r <- tiRand 0 (constant (length (mceChannels a))) t
+  return (select r a)
 
 twChoose :: (UId m) => UGen -> UGen -> UGen -> UGen -> m UGen
-twChoose t a w n = do i <- twindex t n w
-                      return (select i a)
+twChoose t a w n = do
+  i <- twindex t n w
+  return (select i a)
