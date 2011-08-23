ed29e7f49638893dba3c9a7108e53e2c8cd21d50
Rohan Drape <rd@slavepianos.org>
2011-08-23 02:26:55 +0000

    SC3/Identifier (Enum req)

--- Sound/SC3/Identifier.hs
+++ Sound/SC3/Identifier.hs
@@ -3,7 +3,7 @@ module Sound.SC3.Identifier where
 import Data.Char
 
 -- | Typeclass to constrain UGen identifiers.
-class ID a where
+class Enum a => ID a where
     resolveID :: a -> Int
 
 instance ID Int where
