935e5d50c741df13d86de10a9b133a6bd1901327
Rohan Drape <rd@slavepianos.org>
2011-09-23 03:09:44 +0000

    Help/hsc3&tutorial -> hsc3-texts

--- Sound/SC3.hs
+++ Sound/SC3.hs
@@ -1,22 +1,6 @@
-module Sound.SC3 (-- $help
-                  module Sound.SC3.Server
+module Sound.SC3 (module Sound.SC3.Server
                  ,module Sound.SC3.UGen) where
 
 import Sound.SC3.Server
 import Sound.SC3.UGen
-
--- $help
--- Once the hsc3 library has been installed, you will find help files
--- installed in @$PREFIX\/share\/hsc3-VERSION\/Help\/@.
---
--- For installation and configuration information please consult the
--- tutorial file at @Help\/tutorial.lhs@.
---
--- For general information on supercollider and the overall design of
--- the hsc3 bindings, see @Help\/hsc3.help.lhs@.
---
--- For documentation and examples for unit generators, see
--- @Help\/UGen\/@.  Note that the Haddock documentation for many unit
--- generators is incomplete; see the appropriate file in
--- @Help\/UGen\/@ for complete documentation.
 
