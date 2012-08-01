81453ae339e5eb117c1c8f60ec88903fedf9462c
Rohan Drape <rd@slavepianos.org>
2012-08-01 05:56:20 +0000

    ~UId (saner again?)

--- Sound/SC3/UGen/UId.hs
+++ Sound/SC3/UGen/UId.hs
@@ -3,14 +3,19 @@
 module Sound.SC3.UGen.UId where
 
 import Control.Monad
-import Control.Monad.IO.Class
+import Control.Monad.IO.Class as M {- transformers -}
+import Control.Monad.Trans.Reader
 import Data.Unique
+import Sound.OpenSoundControl.Transport.FD as T
 
 -- | A class indicating a monad that will generate a sequence of
 --   unique integer identifiers.
-class (Functor m,MonadIO m) => UId m where
+class (Functor m,M.MonadIO m) => UId m where
    generateUId :: m Int
-   generateUId = fmap hashUnique (liftIO newUnique)
+   generateUId = fmap hashUnique (M.liftIO newUnique)
 
 instance UId IO where
     generateUId = liftM hashUnique newUnique
+
+instance (T.Transport t,Functor io,MonadIO io) => UId (ReaderT t io) where
+   generateUId = ReaderT (M.liftIO . const generateUId)
