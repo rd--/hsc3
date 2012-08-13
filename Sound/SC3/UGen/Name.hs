b95cc19c90c5b5b858edab6c4914f242ababdfde
Rohan Drape <rd@slavepianos.org>
2012-08-13 01:02:02 +0000

    ~tpv(help),~UGen/Name(bpf->BPF)

--- Sound/SC3/UGen/Name.hs
+++ Sound/SC3/UGen/Name.hs
@@ -8,23 +8,30 @@ import Data.Char
 -- > toSC3Name "sinOsc" == "SinOsc"
 -- > toSC3Name "lfSaw" == "LFSaw"
 -- > toSC3Name "pv_Copy" == "PV_Copy"
+-- > map toSC3Name ["bpf","fft","tpv"] == ["BPF","FFT","TPV"]
 toSC3Name :: String -> String
 toSC3Name nm =
     case nm of
-      'l':'f':nm' -> "LF"++nm'
-      'p':'v':'_':nm' -> "PV_"++nm'
-      p:q -> toUpper p : q
+      'l':'f':nm' -> "LF" ++ nm'
+      'p':'v':'_':nm' -> "PV_" ++ nm'
+      p:q -> if all isLower nm && length nm <= 3
+             then map toUpper nm
+             else toUpper p : q
       [] -> []
 
 -- | Inverse of 'toSC3Name'.
 --
 -- > let nm = ["SinOsc","LFSaw","PV_Copy"]
 -- > in map fromSC3Name nm == ["sinOsc","lfSaw","pv_Copy"]
+--
+-- > map fromSC3Name ["BPF","FFT","TPV"] == ["bpf","fft","tpv"]
 fromSC3Name :: String -> String
 fromSC3Name nm =
     case nm of
       'L':'F':nm' -> "lf"++nm'
       'P':'V':'_':nm' -> "pv_"++nm'
-      p:q -> toLower p : q
+      p:q -> if all isUpper nm && length nm <= 3
+             then map toLower nm
+             else toLower p : q
       [] -> []
 
