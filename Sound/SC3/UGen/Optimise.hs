653c5a000383122e55841af27b1857d993df08db
Rohan Drape <rd@slavepianos.org>
2014-06-14 02:23:34 +0000

    ~Sound/SC3/UGen/Optimise(document,spelling)

--- Sound/SC3/UGen/Optimise.hs
+++ Sound/SC3/UGen/Optimise.hs
@@ -1,21 +1,33 @@
 -- | Optimisations of UGen graphs.
 module Sound.SC3.UGen.Optimise where
 
+import System.Random {- random -}
+
 import Sound.SC3.UGen.Operator
 import Sound.SC3.UGen.Rate
 import Sound.SC3.UGen.Type
 import Sound.SC3.UGen.UGen
 
-import System.Random {- random -}
-
+-- | Constant form of 'rand' UGen.
 c_rand :: Random a => Int -> a -> a -> a
 c_rand z l r = fst (randomR (l,r) (mkStdGen z))
 
+-- | Constant form of 'iRand' UGen.
 c_irand :: (Num b, RealFrac a, Random a) => Int -> a -> a -> b
 c_irand z l r = fromInteger (round (c_rand z l r))
 
-ugen_optimize_ir_rand :: UGen -> UGen
-ugen_optimize_ir_rand =
+-- | Optimise 'UGen' graph by re-writing 'rand' and 'iRand' UGens that
+-- have 'Constant' inputs.  This, of course, changes the nature of the
+-- graph, it is no longer randomised at the server.  It's a useful
+-- transformation for very large graphs which are being constructed
+-- and sent each time the graph is played.
+--
+-- > import Sound.SC3.UGen.Dot
+--
+-- > let u = sinOsc AR (rand 'a' 220 440) 0 * 0.1
+-- > in draw (u + ugen_optimise_ir_rand u)
+ugen_optimise_ir_rand :: UGen -> UGen
+ugen_optimise_ir_rand =
     let f u =
             case u of
               Primitive_U p ->
@@ -28,8 +40,18 @@ ugen_optimize_ir_rand =
               _ -> u
     in ugenTraverse f
 
-ugen_optimize_const_binop :: UGen -> UGen
-ugen_optimize_const_binop =
+-- | Optimise 'UGen' graph by re-writing binary operators with
+-- 'Constant' inputs.  The standard graph constructors already do
+-- this, however subsequent optimisations, ie. 'ugen_optimise_ir_rand'
+-- can re-introduce these sub-graphs.
+--
+-- > constant 5 * 10 == constant 50
+--
+-- > let {u = lfPulse AR (2 ** rand 'α' (-9) 1) 0 0.5
+-- >     ;u' = ugen_optimise_ir_rand u}
+-- > in draw (mix (mce [u,u',ugen_optimise_const_binop u']))
+ugen_optimise_const_binop :: UGen -> UGen
+ugen_optimise_const_binop =
     let f u =
             case u of
               Primitive_U p ->
