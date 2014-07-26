997757e76e46ff12320e03bffa0265d3502cb8b7
Rohan Drape <rd@slavepianos.org>
2014-07-26 07:09:51 +0000

    ~UGen/Envelope(+doc)

--- Sound/SC3/UGen/Envelope.hs
+++ Sound/SC3/UGen/Envelope.hs
@@ -126,6 +126,8 @@ envelope_curves e =
 
 -- | Linear SC3 form of 'Envelope' data.
 --
+-- Form is: l0 #t reset loop l1 t0 c0 c0' ...
+--
 -- > let {l = [0,0.6,0.3,1.0,0]
 -- >     ;t = [0.1,0.02,0.4,1.1]
 -- >     ;c = [EnvLin,EnvExp,EnvNum (-6),EnvSin]
