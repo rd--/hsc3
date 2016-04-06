cf65a001d451952067df767c700408dc1db2767d
Rohan Drape <rd@slavepianos.org>
2016-04-06 07:46:07 +0000

    ~Sound.SC3 (export Common.Math)

--- Sound/SC3.hs
+++ Sound/SC3.hs
@@ -1,8 +1,8 @@
--- | Composite of "Sound.SC3.Server.Monad", "Sound.SC3.UGen" and "Sound.SC3.UGen.Bindings".
+-- | Composite of "Sound.SC3.Common.Math", "Sound.SC3.Server.Monad",
+-- "Sound.SC3.UGen" and "Sound.SC3.UGen.Bindings".
 module Sound.SC3 (module M) where
 
+import Sound.SC3.Common.Math as M
 import Sound.SC3.Server.Monad as M
 import Sound.SC3.UGen as M
-
 import Sound.SC3.UGen.Bindings as M
-
