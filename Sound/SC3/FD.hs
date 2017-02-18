b4f62cad66304e10fd8e052f69527a88966bf9e4
Rohan Drape <rd@slavepianos.org>
2017-02-18 23:46:02 +0000

    ~Sound/SC3/FD (missing sub-module...)

--- Sound/SC3/FD.hs
+++ Sound/SC3/FD.hs
@@ -1,6 +1,7 @@
 -- | Composite of "Sound.SC3.Server.FD" and "Sound.SC3.UGen" and "Sound.SC3.UGen.Bindings".
 module Sound.SC3.FD (module M) where
 
+import Sound.SC3.Common.Buffer as M
 import Sound.SC3.Common.Math as M
 import Sound.SC3.Server.FD as M
 import Sound.SC3.UGen as M
