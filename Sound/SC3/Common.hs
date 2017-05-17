2cf52600d7061a83e884cd1df2727239c0195bf9
Rohan Drape <rd@slavepianos.org>
2017-05-17 23:38:50 +0000

    ~Sound/SC3/Common.hs

--- Sound/SC3/Common.hs
+++ Sound/SC3/Common.hs
@@ -1,1 +1,6 @@
-module Sound.SC3.Common where
+module Sound.SC3.Common (module M) where
+
+import Sound.SC3.Common.Buffer as M
+import Sound.SC3.Common.Envelope as M
+import Sound.SC3.Common.Math as M
+import Sound.SC3.Common.Monad as M
--- Sound/SC3/FD.hs
+++ Sound/SC3/FD.hs
@@ -1,8 +1,8 @@
--- | Composite of "Sound.SC3.Server.FD" and "Sound.SC3.UGen" and "Sound.SC3.UGen.Bindings".
+-- | Composite of "Sound.SC3.Common", "Sound.SC3.Server.FD",
+--   "Sound.SC3.UGen" and "Sound.SC3.UGen.Bindings".
 module Sound.SC3.FD (module M) where
 
-import Sound.SC3.Common.Buffer as M
-import Sound.SC3.Common.Math as M
+import Sound.SC3.Common as M
 import Sound.SC3.Server.FD as M
 import Sound.SC3.UGen as M
 import Sound.SC3.UGen.Bindings as M
