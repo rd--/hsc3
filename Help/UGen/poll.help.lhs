63f165bebda10867be397e96c64f7d2dc114a283
Rohan Drape <rd@slavepianos.org>
2018-01-03 22:49:23 +0000

    ~poll/rw

--- Help/UGen/poll.help.lhs
+++ Help/UGen/poll.help.lhs
@@ -6,11 +6,11 @@
 > g_01 =
 >     let t = impulse KR 10 0
 >         l = line KR 0 1 1 RemoveSynth
->     in poll t l (label "polling...") 0
+>     in poll t l 0 (label "polling...")
 
 multichannel expansion (requires labels be equal length...)
 
 > g_02 =
 >     let t = impulse KR (mce2 10 5) 0
 >         l = line KR 0 (mce2 1 5) (mce2 1 2) DoNothing
->     in poll t l (mce2 (label "t1") (label "t2")) 0
+>     in poll t l 0 (mce2 (label "t1") (label "t2"))
--- Sound/SC3/UGen/Bindings/HW.hs
+++ Sound/SC3/UGen/Bindings/HW.hs
@@ -33,7 +33,10 @@ packFFT b sz from to z mp =
 
 -- | Poll value of input UGen when triggered.
 poll :: UGen -> UGen -> UGen -> UGen -> UGen
-poll trig_ in_ label_ trigid = C.mkFilter "Poll" ([trig_,in_,label_] ++ U.unpackLabel trigid) 0
+poll trig_ in_ trigid label_ =
+  let q = U.unpackLabel label_
+      n = fromIntegral (length q)
+  in C.mkFilter "Poll" ([trig_,in_,trigid,n] ++ q) 0
 
 -- | Send a reply message from the server back to all registered clients.
 sendReply :: UGen -> UGen -> String -> [UGen] -> UGen
