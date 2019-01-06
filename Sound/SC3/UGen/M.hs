686375b7bd95a0dc4951fd9534f8ab1e285c5601
rohan <rd>
2019-01-06 22:42:28 +1100

    ~UGen.M,-why_sc3

--- Sound/SC3/UGen/M.hs
+++ Sound/SC3/UGen/M.hs
@@ -3,7 +3,6 @@ module Sound.SC3.UGen.M where
 import Control.Monad.Trans.State.Lazy {- transformers -}
 import Data.Functor.Identity {- base -}
 
-import Sound.SC3.Common.Base {- hsc3 -}
 import Sound.SC3.Common.UId {- hsc3 -}
 import Sound.SC3.UGen.Rate {- hsc3 -}
 
@@ -117,15 +116,3 @@ audition (UGen u) = SC3.audition (uid_st_eval u)
 -- > audition t0
 t0 :: UId m => UGen m
 t0 = out 0 (mce2 (sinOsc AR 440 0 * 0.1) ((whiteNoise AR - whiteNoise AR) * 0.1))
-
--- > draw why_supercollider
--- > audition why_supercollider
-why_supercollider :: UId m => UGen m
-why_supercollider =
-    let r = resonz (dust AR 0.2 * 50) (rand 200 3200) 0.003
-        s = sum (replicate 10 r)
-        c = combL (delayN s 0.048 0.048) 0.1 (lfNoise1 KR (rand 0 0.1) * 0.04 + 0.05) 15
-        y = sum (replicate 7 c)
-        f i = allpassN i 0.05 (mce2 (rand 0 0.05) (rand 0 0.05)) 1
-        x = compose_l (replicate 4 f) y
-    in s + 0.2 * x
