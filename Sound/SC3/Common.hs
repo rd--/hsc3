0185cc43ddead70a7bfd5ffa75ddf01d88ecb24c
rohan <rd>
2019-01-08 21:57:02 +1100

    ~common

--- Sound/SC3/Common.hs
+++ Sound/SC3/Common.hs
@@ -2,6 +2,7 @@
 module Sound.SC3.Common (module M) where
 
 import Sound.SC3.Common.Buffer as M
+import Sound.SC3.Common.Enum as M
 import Sound.SC3.Common.Envelope as M
 import Sound.SC3.Common.Math as M
 import Sound.SC3.Common.Monad as M
