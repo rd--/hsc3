2ed67cfc67457d3496660144809850cab69b059d
rohan <rd>
2019-01-10 14:25:19 +1100

    export Sound/SC3/Common/Math/Operator

--- Sound/SC3/Common.hs
+++ Sound/SC3/Common.hs
@@ -5,6 +5,7 @@ import Sound.SC3.Common.Buffer as M
 import Sound.SC3.Common.Enum as M
 import Sound.SC3.Common.Envelope as M
 import Sound.SC3.Common.Math as M
+import Sound.SC3.Common.Math.Operator as M
 import Sound.SC3.Common.Monad as M
 import Sound.SC3.Common.Rate as M
 import Sound.SC3.Common.UId as M
