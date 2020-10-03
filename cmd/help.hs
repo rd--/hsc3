008a63661e72996fe9c8f64037308bdd63b8784f
rohan <rd>
2020-10-03 11:54:14 +1000

    ~cmd/help,+rtf

--- cmd/help.hs
+++ cmd/help.hs
@@ -1,15 +1,21 @@
 import System.Environment {- base -}
 
-import Sound.SC3.UGen.DB {- hsc3-db -}
+import qualified Sound.SC3.Lang.Help as Help {- hsc3-lang -}
+
+import qualified Sound.SC3.UGen.DB as DB {- hsc3-db -}
 
 help :: [String]
 help =
     ["hsc3-help command [arguments]"
+    ," sc3-help {rtf|scdoc-local|scdoc-online} subject..."
     ," ugen-summary ugen-name..."]
 
 main :: IO ()
 main = do
   a <- getArgs
   case a of
-    "ugen-summary":u -> mapM_ ugenSummary u
+    "sc3-help":"rtf":x -> mapM_ Help.sc3_rtf_help_scd_open_emacs x
+    "sc3-help":"scdoc-local":x -> mapM_ (Help.sc3_scdoc_help_open True . Help.sc3_scdoc_help_path) x
+    "sc3-help":"scdoc-online":x -> mapM_ (Help.sc3_scdoc_help_open False . Help.sc3_scdoc_help_path) x
+    "ugen-summary":u -> mapM_ DB.ugenSummary u
     _ -> putStrLn (unlines help)
