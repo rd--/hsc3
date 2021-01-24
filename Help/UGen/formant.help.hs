f05ff804303c2eb714c38035296597cd35abd997
rohan <rd>
2021-01-24 15:17:20 +1100

    ~help/formant,-cmd/cabal,

--- Help/UGen/formant.help.hs
+++ Help/UGen/formant.help.hs
@@ -7,7 +7,7 @@ formant AR (xLine KR 400 1000 8 RemoveSynth) 2000 800 * 0.125
 -- formant ; modulate formant frequency, fundamental frequency stays constant
 let f = mce [200, 300, 400, 500]
     ff = xLine KR 400 4000 8 RemoveSynth
-in formant AR f ff 200 * 0.125
+in splay (formant AR f ff 200) 1 1 0 True * 0.125
 
 -- formant ; modulate width frequency, other frequencies stay constant
 let bw = xLine KR 800 8000 8 RemoveSynth
@@ -16,6 +16,6 @@ in formant AR 400 2000 bw * 0.1
 -- formant ; event control
 let f _ (g,x,y,z,o,_,_,_) =
       let f0 = mce [200, 300, 400, 500] * x
-          ff = linExp y 0 1 400 4000
-      in pan2 (formant AR f0 ff 200) (o * 2 - 1) (g * z)
+          ff = linExp y 0 1 400 1200
+      in splay (formant AR f0 ff 200) 1 (g * z) (o * 2 - 1) True
 in mix (rEventVoicer 16 f) * control KR "gain" 0.5
--- Help/UGen/grainSin.help.hs
+++ Help/UGen/grainSin.help.hs
@@ -13,3 +13,9 @@ let k = 16
     l = mce (map f [0::Int .. k - 1])
 in grainSin 2 t (mouseX KR 0.001 0.5 Exponential 0.2) (tChoose 'α' t l) 0 (-1) 512 * 0.1
 
+-- grainSin ; event control
+let f _ (g,_,y,z,o,rx,ry,p) =
+      let tr = impulse AR (linLin y 0 1 6 72) 0
+          du = linLin rx 0 1 0.01 0.15
+      in grainSin 2 tr du (midiCPS (p + (ry * 2 - 1))) (o * 2 - 1) (-1) 512 * z * g
+in mix (rEventVoicer 16 f) * control KR "gain" 1
