08130a82b9bf76382f7ffb177a6fdd60a640d20b
rohan <rd>
2021-02-01 15:24:07 +1100

    ~help/lpf,+event

--- Help/UGen/lpf.help.hs
+++ Help/UGen/lpf.help.hs
@@ -6,3 +6,9 @@ in lpf (saw AR 200 * 0.1) ff
 -- lpf ; control rate filtering
 let ctl = lpf (lfPulse KR 8 0 0.5) (mouseX KR 2 50 Exponential 0.1)
 in sinOsc AR (ctl * 200 + 400) 0 * 0.1
+
+-- lpf ; event control
+let f _ (g,_,y,z,o,rx,ry,p) =
+      let f0 = midiCPS p
+      in pan2 (lpf (blip AR f0 (1 + y * 5)) (f0 * 0.25) * 4) (o * 2 - 1) (lagUD g 0.05 1 * z)
+in mix (rEventVoicer 16 f) * control KR "gain" 2
--- Help/UGen/rlpf.help.hs
+++ Help/UGen/rlpf.help.hs
@@ -28,5 +28,5 @@ let f _ (g,_,y,z,o,rx,ry,p) =
       let f0 = midiCPS p
           f1 = f0 * 0.5 * (1 + y * 6)
           rq = linLin (rx * ry) 0 0.25 0.1 0.6
-      in pan2 (rlpf (lfTri AR f0 0) f1 rq) (o * 2 - 1) (lagUD g 0.05 (2 - y) * z)
-in mix (rEventVoicer 16 f) * control KR "gain" 0.5
+      in pan2 (rlpf (lfTri AR f0 0) f1 rq) (o * 2 - 1) (lagUD g 0.05 (2 - y) * (2 - y) * z)
+in mix (rEventVoicer 16 f) * control KR "gain" 0.25
