6c80711de645ebdced40e1f4144cd41483af4b0c
rohan <rd>
2021-03-09 16:26:58 +1100

    +Help/UGen/weaklyNonlinear2.help.hs

--- Help/UGen/blip.help.hs
+++ Help/UGen/blip.help.hs
@@ -13,7 +13,14 @@ let fr = blip KR 0.25 3 * 300 + 500
 in blip AR fr nh * 0.2
 
 -- blip ; event control
-let f _ (g,_,y,z,o,_,_,p,px,_) = pan2 (blip AR (midiCPS (p + px)) (y * 10 + 1)) (o * 2 - 1) (g * z)
+let f _ (g,x,y,z,o,_,_,_,_,_) = pan2 (blip AR (midiCPS (x * 13 + 48)) (y * 10 + 1)) (o * 2 - 1) (g * z)
+in mix (rEventVoicer 16 f) * control KR "gain" 0.5
+
+-- blip ; event control (p)
+let f _ (g,_,_,z,o,_,_,p,px,py) =
+      let f0 = midiCPS (p + px)
+          nh = max 0 py * 10 + 1
+      in pan2 (blip AR f0 nh) (o * 2 - 1) (g * z)
 in mix (rEventVoicer 16 f) * control KR "gain" 0.5
 
 ---- ; drawings
--- Help/UGen/pluck.help.hs
+++ Help/UGen/pluck.help.hs
@@ -44,6 +44,6 @@ let f _ (g,_,y,z,o,_,_,p,px,_) =
       let n = whiteNoise 'α' AR * z
           dl_max = 1 / 8
           (gt,tr) = rEventGateReset g p
-          dl = 1 / midiCPS (p + px)
+          dl = 1 / midiCPS (p + px * 1)
       in pan2 (pluck n gt dl_max dl 10 (y / 3)) (o * 2 - 1) 1
 in mix (rEventVoicer 16 f) * control KR "gain" 2
