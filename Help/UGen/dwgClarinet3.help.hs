d9999be0aa2b11f7cbb4c1266f46a26741f60d8c
rohan <rd>
2021-03-21 09:48:55 +1100

    +dwg/event,initial

--- Help/UGen/dwgClarinet3.help.hs
+++ Help/UGen/dwgClarinet3.help.hs
@@ -14,3 +14,17 @@ let freq = control_m KR "freq" 440 (220,880,"exp")
     pm = envGen AR gate_ 1 0 1 DoNothing (envASR att 1 0.2 (EnvNum 1)) * amp
     signal = X.dwgClarinet3 AR (lag freq 0 * vib) pm pc m 1 release c1 c3
 in pan2 (hpf signal 200) pan amp
+
+-- dwgClarinet3 ; event control
+let f _ (g,x,y,z,o,rx,ry,_,_,_) =
+      let freq = midiCPS (x * 25 + 42)
+          vib = sinOsc KR (y * 4) 0 * (z * 0.005) + 1
+          amp = linLin z 0 1 0.65 1
+          pm = k2a (amp * g)
+          pc = linLin y 0 1 0.25 0.85
+          m = linLin rx 0 1 0.4 1.25
+          c1 = ry
+          c3 = linLin o 0 1 5 9
+          signal = X.dwgClarinet3 AR (freq * vib) pm pc m 1 0.01 c1 c3
+      in pan2 (hpf signal 200) (o * 2 - 1) 1
+in mix (rEventVoicer 16 f) * control KR "gain" 0.25
--- Help/UGen/dwgFlute.help.hs
+++ Help/UGen/dwgFlute.help.hs
@@ -13,3 +13,15 @@ let freq = control_m KR "freq" 440 (220,880,"exp")
     pm = envGen AR gate_ 1 0 1 DoNothing (envASR att 1 0.2 (EnvNum 1)) * (1.1 + (amp * 0.2)) * vib
     signal = X.dwgFlute AR freq pm endr jetr jetRa 1 release
 in pan2 (hpf (lpf signal 6000) 200) pan (0.2 * amp)
+
+-- dwgFlute ; event control
+let f c (g,x,y,z,o,rx,ry,_,_,_) =
+      let freq = midiCPS (x * 25 + 42)
+          vib = sinOsc KR 4 0 + (0.01 * z * whiteNoise c AR)
+          pm = k2a ((0.1 + z) * vib * g)
+          endr = linLin ry 0 1 0.35 0.65
+          jetr = linLin y 0 1 0.65 0.85 * lagUD g 0.001 0.25
+          jetRa = linLin rx 0 1 0.25 0.85 * lagUD g 0.001 0.25
+          signal = X.dwgFlute AR freq pm endr jetr jetRa 1 0.01
+      in pan2 (hpf (lpf signal 6000) 200) (o * 2 - 1) 1
+in mix (rEventVoicer 16 f) * control KR "gain" 0.25
