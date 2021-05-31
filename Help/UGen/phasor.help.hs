51f151fcbfb3934cb1d0d819c33fea735ea7970e
rohan <rd>
2021-05-31 13:24:49 +1000

    ~Help/UGen/phasor

--- Help/UGen/phasor.help.hs
+++ Help/UGen/phasor.help.hs
@@ -52,6 +52,17 @@ let f0 = 220
     ph = phasor AR (impulse AR f0 0) (two_pi * f0 / sampleRate) 0 two_pi 0
 in sin ph * 0.1
 
+-- phasor ; approximation of square wave ; sync ; http://listarc.bham.ac.uk/lists/sc-users/msg69869.html
+let freq = mouseX KR 200 4000 Exponential 0.2
+    syncFreq = mouseY KR 50 800 Exponential 0.2
+    syncSig = sinOsc AR syncFreq 0
+    phase = phasor AR syncSig (freq * sampleDur) 0 1 0 * 2 * pi
+    k = 12000 * (sampleRate / 48000) / (freq * log10 freq)
+    width = sinOsc KR 0.1 0 `in_range` (0,1)
+    sinSig = sinOsc AR 0 phase - (width * 2 - 1)
+    squareSig = tanh (sinSig * k)
+in pan2 (leakDC squareSig 0.995) 0 0.05
+
 ---- ; load sound file to buffer zero
 withSC3 (async (b_allocRead 0 "/home/rohan/data/audio/pf-c5.aif" 0 0))
 
