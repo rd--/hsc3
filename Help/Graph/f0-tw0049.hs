3f2b41eee3f429a390f19ca21f08acce8dea3762
rohan <rd>
2021-06-10 16:33:18 +1000

    ~minor

--- Help/Graph/f0-tw0049.hs
+++ Help/Graph/f0-tw0049.hs
@@ -3,5 +3,5 @@ let t = t2a 0 0
     o0 = lfSaw AR 3 0 * 9 + 99
     s0 = sweep t (mce [3..9]) `modE` o0
     s1 = sweep t (mce [33,32..3]) `modE` 128 .&. s0
-    o1 = sinOsc AR (midiCPS (s1+33)) 0 * pi
+    o1 = sinOsc AR (midiCPS (s1 + 33)) 0 * pi
 in splay (sinOsc AR 9 o1) 1 1 0 True / 12
--- Help/Graph/nv-tw-2013-12-04.hs
+++ Help/Graph/nv-tw-2013-12-04.hs
@@ -1,7 +1,7 @@
 -- https://twitter.com/headcube/status/408145586970324992 (nv)
 let mean l = sum l / fromIntegral (length l)
     f j =
-      let i = j + 1;
+      let i = j + 1
           a = saw AR ((1 / i + 1) / 6)
           p = pluck a a 1 (1 / i / (3 - lfPulse AR (1 / i) 0 0.5) / 30) 9 (0.9 / i)
           x = (0.5 ** i) * p
