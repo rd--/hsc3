d3daa759bdcec16b0b9f8a73954e75e79ffaf7eb
rohan <rd>
2021-06-16 15:02:34 +1000

    ~help,+klar

--- Help/Graph/mn-k2ws.hs
+++ Help/Graph/mn-k2ws.hs
@@ -1,12 +1,10 @@
 -- https://www.listarc.bham.ac.uk/lists/sc-users/msg21341.html (mn)
-let rng :: (UGen,UGen) -> UGen -> UGen
-    rng (i,j) = range i j
-    fc = 400 + rng (100,200) (lfNoise2 'α' AR (mce2 1 2))
-    fm = rng (100,200) (lfNoise0 'β' AR 5)
-    i = rng (1,20) (lfNoise1 'γ' AR 10)
+let fc = 400 + (lfNoise2 'α' AR (mce2 1 2) `in_range` (100,200))
+    fm = lfNoise0 'β' AR 5 `in_range` (100,200)
+    i = lfNoise1 'γ' AR 10 `in_range` (1,20)
     x = sinOsc AR (fc + (sinOsc AR fm 0 * i * fm)) 0 * 0.5
-    rf = rng (1000,2000) (sinOsc AR (rng (0.1,1) (lfNoise1 'δ' AR 1)) 0)
-    rq = rng (0.5,10) (lfNoise1 'ε' AR 1)
+    rf = sinOsc AR (lfNoise1 'δ' AR 1 `in_range` (0.1,1)) 0 `in_range` (1000,2000)
+    rq = lfNoise1 'ε' AR 1 `in_range` (0.5,10)
     a = rlpf x rf rq
-    ph = a * rng (1.0,4.0) (lfNoise1 'ζ' AR 0.1) * 4 * pi
+    ph = a * (lfNoise1 'ζ' AR 0.1 `in_range` (1.0,4.0)) * 4 * pi
 in sinOsc AR 0.2 ph * 0.1
--- Help/Graph/tm-tw-463992770596577280.hs
+++ Help/Graph/tm-tw-463992770596577280.hs
@@ -2,5 +2,5 @@
 let f z i =
       let x = constant i + 1
           e = lfNoise2 z AR 0.5 * line AR 0 0.1 (rand z 1 99) DoNothing / (x * 0.2)
-      in sinOsc AR (30 * x + linLin_b (lfNoise2 z AR 0.1) (-2) 2) 0 * e
+      in sinOsc AR (30 * x + lfNoise2 z AR 0.1 * 2) 0 * e
 in mceFill_z 'α' 2 (\z _ -> mixFill_z z 24 f)
