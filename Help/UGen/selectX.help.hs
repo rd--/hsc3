9f80efb4d137568db01bc82920492949997fe913
rohan <rd>
2021-06-18 13:25:34 +1000

    +lib/repl,init

--- Help/UGen/selectX.help.hs
+++ Help/UGen/selectX.help.hs
@@ -8,5 +8,5 @@ in mix (selectX (lfSaw KR 1 0 * n + n) a * 0.2)
 let n = 10
     a = mce [517, 403, 89, 562, 816, 107, 241, 145, 90, 224]
     c = n / 2
-    f = mix (selectX (lfSaw KR 0.5 0 * c + c) a)
+    f = selectX (lfSaw KR 0.5 0 * c + c) a
 in saw AR f * 0.2
