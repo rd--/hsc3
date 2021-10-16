71ad6e48ebf886f5383356eca8214a6493e5bac6
rohan <rd@slavepianos.org>
2021-10-16 19:50:25 +1100

    ~brackets,betterdoc

--- Sound/SC3/UGen/Bracketed.hs
+++ Sound/SC3/UGen/Bracketed.hs
@@ -1,11 +1,12 @@
 {- | Bracketed UGens.
 
-'Brackets' are a pair of OpenSoundControl message sequences that can be attached to UGen values.
-One sequence (called /pre/) is to be sent before the graph the UGen belongs to is started,
-the other (called /post/) is to be sent afterwards.
-The functions defined here return UGens with Brackets.
-They're designed so that they can be used to write ordinary graphs with ordinary control parameters.
-The brackets contain instructions that would otherwise be given and executed outside of the graph context.
+UGen brackets are a pair of OpenSoundControl message sequences that are attached to UGen values.
+The first sequence is to be sent before the graph the UGen belongs to is started, the other after it has ended.
+The brackets contain instructions that would otherwise be written outside of the graph context.
+The functions defined here return UGen values with brackets attached to them.
+It is possible to use these functions to write the same graphs as would be written without the brackets,
+i.e. with control and channel count inputs.
+scsynthPlayAt will read and send UGen bracket messages.
 -}
 module Sound.SC3.UGen.Bracketed where
 
@@ -18,8 +19,11 @@ import Sound.SC3.UGen.UGen {- hsc3 -}
 import Sound.SC3.Server.Command.Plain {- hsc3 -}
 import Sound.SC3.Server.Enum {- hsc3 -}
 
-{- | Number of channels, either sfNc or the length of readChan.
-     Checks requested channels are in range.
+{- | sfNc is the number of channels at a sound file.
+     readChan is a list of channels indexed to read.
+     Returns a channel count, either sfNc or the length of readChan.
+     If readChan in empty returns sfNc, else returns the length of readChan.
+     This function checks that requested channels are in range.
 -}
 readChanToNc :: Int -> [Int] -> Int
 readChanToNc sfNc readChan =
@@ -29,9 +33,9 @@ readChanToNc sfNc readChan =
        then length readChan
        else error "readChanToNc: channel error"
 
-{- | diskIn or vDiskIn with brackets to allocate and read and then close and free buffer.
-     Ignoring the brackets, this is equivalent to writing a diskIn UGen with
-     the number of channels derived from the named file.
+{- | diskIn or vDiskIn with brackets to 1. allocate and read and then 2. close and free buffer.
+     Ignoring the brackets, this is equivalent to writing a diskIn UGen,
+     with the number of channels given by readChan or derived from the named file.
      If readChan is empty all channels are read.
 -}
 sndfileDiskIn :: (Buffer_Id, String, [Int]) -> FilePath -> Maybe UGen -> Loop UGen -> UGen
@@ -54,12 +58,12 @@ sndfileIn opt sndFileName loop = sndfileDiskIn opt sndFileName Nothing loop
 sndfileVarIn :: (Buffer_Id, String, [Int]) -> FilePath -> UGen -> Loop UGen -> UGen
 sndfileVarIn opt sndFileName rate loop = sndfileDiskIn opt sndFileName (Just rate) loop
 
-{- | Returns Buffer_Id as a bracketed buffer identifier UGen
+{- | Returns Buffer_Id as a bracketed buffer identifier UGen,
      along with basic sound file information: numberOfChannels, sampleRate, numberOfFrames.
      If ctlName is empty the buffer is returned as a constant, else as a control with the given name.
-     The brackets will allocate and read and then free the buffer.
+     The brackets will 1. allocate and read and then 2. free the buffer.
      Ignoring the brackets, and the sample rate and frame count,
-     this can be used to write the synthdefs that function the same as if just having a buffer control input.
+     this can be used to write the synthdefs that are the same as if just having a buffer control input.
      If readChan is empty all channels are read.
 -}
 sndfileRead :: (Buffer_Id, String, [Int]) -> FilePath -> (UGen, Int, UGen, UGen)
@@ -70,25 +74,29 @@ sndfileRead (bufId, ctlName, readChan) sndFileName =
       bufNc = readChanToNc sfNc readChan
   in (bracketUGen buf ([b_allocReadChannel bufId fileName 0 0 readChan], [b_free bufId]), bufNc, constant sfSr, constant sfNf)
 
--- | Bracketed b_gen sine1
+{- | Bracketed b_gen sine1
+     If ctlName is empty the buffer is returned as a constant, else as a control with the given name.
+-}
 bGenSine1 :: (Buffer_Id, String, Int) -> [B_Gen] -> [Double] -> UGen
 bGenSine1 (bufId, ctlName, numFrames) flags param =
   let buf = if null ctlName then constant bufId else control kr ctlName (fromIntegral bufId)
       bufNc = 1
   in bracketUGen buf ([b_alloc bufId numFrames bufNc, b_gen_sine1 bufId flags param], [b_free bufId])
 
--- | bGenSine1 with standard wavetable flags.
+-- | bGenSine1 with standard wavetable flags (normalise and wavetable and clear).
 bGenSine1Tbl :: (Buffer_Id, String, Int) -> [Double] -> UGen
 bGenSine1Tbl opt = bGenSine1 opt [Normalise, Wavetable, Clear]
 
--- | Bracketed b_gen sine1
+{- | Bracketed b_gen sine1
+     If ctlName is empty the buffer is returned as a constant, else as a control with the given name.
+-}
 bGenCheby :: (Buffer_Id, String, Int) -> [B_Gen] -> [Double] -> UGen
 bGenCheby (bufId, ctlName, numFrames) flags param =
   let buf = if null ctlName then constant bufId else control kr ctlName (fromIntegral bufId)
       bufNc = 1
   in bracketUGen buf ([b_alloc bufId numFrames bufNc, b_gen_cheby bufId flags param], [b_free bufId])
 
--- | bGenCheby with standard wavetable flags.
+-- | bGenCheby with standard wavetable flags (normalise and wavetable and clear).
 bGenChebyTbl :: (Buffer_Id, String, Int) -> [Double] -> UGen
 bGenChebyTbl opt = bGenCheby opt [Normalise, Wavetable, Clear]
 
