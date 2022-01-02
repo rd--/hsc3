9dc9c00fa28a8c2ff83b61dc9dcc87558049aae3
rohan <rd@slavepianos.org>
2022-01-02 13:10:42 +1100

    tidying

--- Help/Graph/cr-lucier.hs
+++ Help/Graph/cr-lucier.hs
@@ -1,7 +1,7 @@
 -- Alvin Lucier, "Music on a Long Thin Wire, Simulated" (Chandrasekhar Ramakrishnan)
 let freq = control kr "freq" 60
-    rcr = recip controlRate
-    mk_dt f = recip f - rcr
+    blocksize = recip controlRate
+    mk_dt f = recip f - blocksize
     string_delay = mk_dt freq
     pk1_pos = 0.1 -- pickup one position
     src_pos = 0.3 -- source position
@@ -10,35 +10,16 @@ let freq = control kr "freq" 60
     mk_delay i r = lpz1 (delayC i max_delay (r * string_delay))
     mk_allpass i r dt = lpz1 (allpassC i max_delay (r * string_delay) dt)
     drv = localIn 1 ar 0 -- driver (source + data stored in the string)
-    pk1_R =
-        let i = drv
-            r = src_pos - pk1_pos
-        in mk_delay i r
-    pk1_L =
-        let i = pk1_R * negate 1
-            r = pk1_pos * 2
-            dt = rand 0.001 0.11
-        in mk_allpass i r dt
-    pk2_L =
-        let i = pk1_L
-            r = pk2_pos - pk1_pos
-        in mk_delay i r * 0.99
-    stringL =
-        let i = pk2_L
-            r = 1.0 - pk2_pos
-        in mk_delay i r
-    pk2_R =
-        let i = stringL * negate 1
-            r = 1.0 - pk2_pos
-            dt = 2 + rand 0.001 0.11
-        in mk_allpass i r dt * 0.99
-    stringR =
-        let i = pk2_R
-            r = pk2_pos - src_pos
-        in mk_delay i r
+    pk1_R = mk_delay drv (src_pos - pk1_pos)
+    pk1_L = mk_allpass (pk1_R * negate 1) (pk1_pos * 2) (rand 0.001 0.11)
+    pk2_L = mk_delay pk1_L (pk2_pos - pk1_pos) * 0.99
+    stringL = mk_delay pk2_L (1.0 - pk2_pos)
+    pk2_R = mk_allpass (stringL * negate 1) (1.0 - pk2_pos) (2 + rand 0.001 0.11) * 0.99
+    stringR = mk_delay pk2_R (pk2_pos - src_pos)
     source =
         let s = sinOsc ar 220 0 * 0.01
-            p = pulse ar (60 + amplitude kr drv 0.01 0.01 * 11) 0.5 * 0.1
+            a = amplitude kr drv 0.01 0.01 * 11
+            p = pulse ar (60 + a) 0.5 * 0.1
             f = rlpf (s + p) 320 0.05
             e = 1.0 - min (amplitude kr drv 0.01 0.01) 1.0
         in normalizer f 0.7 0.01 * e
@@ -49,8 +30,8 @@ in mrg [mce2 outL outR, drv, source, l_out]
 
 -- Alvin Lucier, "Music on a Long Thin Wire, Simulated" (Chandrasekhar Ramakrishnan) ; id
 let freq = control kr "freq" 60
-    rcr = recip controlRate
-    mk_dt f = recip f - rcr
+    blocksize = recip controlRate
+    mk_dt f = recip f - blocksize
     string_delay = mk_dt freq
     pk1_pos = 0.1 -- pickup one position
     src_pos = 0.3 -- source position
--- Help/UGen/selectX.help.hs
+++ Help/UGen/selectX.help.hs
@@ -2,11 +2,11 @@
 let n = 3/2
     f = mce2 440 441
     a = mce [sinOsc ar f 0, saw ar f, pulse ar f 0.1]
-in mix (selectX (lfSaw kr 1 0 * n + n) a * 0.2)
+in mix (selectX (lfSaw kr 1 0 * n + n) a * 0.1)
 
 -- selectX ; as sequencer
 let n = 10
     a = mce [517, 403, 89, 562, 816, 107, 241, 145, 90, 224]
     c = n / 2
     f = selectX (lfSaw kr 0.5 0 * c + c) a
-in saw ar f * 0.2
+in saw ar f * 0.1
