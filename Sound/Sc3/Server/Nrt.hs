ee04355ab6c8005cf286ead5eaa5dbf59dd2a3e7
rohan <rd@slavepianos.org>
2025-06-07 14:18:02 +1000

    nicerNrtHelp

--- Sound/Sc3/Server/Nrt.hs
+++ Sound/Sc3/Server/Nrt.hs
@@ -5,74 +5,78 @@ import Data.List {- base -}
 import Data.Maybe {- base -}
 import System.IO {- base -}
 
-import qualified Data.ByteString.Lazy as B {- bytestring -}
+import qualified Data.ByteString.Lazy as ByteString.Lazy {- bytestring -}
 
 import qualified Sound.Osc.Coding.Byte as Byte {- hosc -}
-import qualified Sound.Osc.Coding.Decode.Binary as Decode {- hosc3 -}
-import qualified Sound.Osc.Coding.Encode.Builder as Encode {- hosc3 -}
-import Sound.Osc.Datum {- hosc -}
+import qualified Sound.Osc.Coding.Decode.Binary as Decode {- hosc -}
+import qualified Sound.Osc.Coding.Encode.Builder as Encode {- hosc -}
+import qualified Sound.Osc.Datum as Datum {- hosc -}
 import Sound.Osc.Packet {- hosc -}
 
 -- | Encode Bundle and prefix with encoded length.
-oscWithSize :: BundleOf Message -> B.ByteString
+oscWithSize :: BundleOf Message -> ByteString.Lazy.ByteString
 oscWithSize o =
   let b = Encode.encodeBundle o
-      l = Byte.encode_i32 (fromIntegral (B.length b))
-  in B.append l b
+      l = Byte.encode_i32 (fromIntegral (ByteString.Lazy.length b))
+  in ByteString.Lazy.append l b
 
 -- | An 'Nrt' score is a sequence of 'Bundle's.
-newtype Nrt = Nrt {nrt_bundles :: [BundleOf Message]} deriving (Show)
+newtype Nrt = Nrt {nrt_bundles :: [BundleOf Message]} deriving (Eq, Show)
 
 {- | 'span' of 'f' of 'bundleTime'.
      Can be used to separate the /initialisation/ and /remainder/ parts of a score.
 -}
-nrt_span :: (Time -> Bool) -> Nrt -> ([BundleOf Message], [BundleOf Message])
+nrt_span :: (Datum.Time -> Bool) -> Nrt -> ([BundleOf Message], [BundleOf Message])
 nrt_span f = span (f . bundleTime) . nrt_bundles
 
--- | Encode an 'Nrt' score.
-encodeNrt :: Nrt -> B.ByteString
-encodeNrt = B.concat . map oscWithSize . nrt_bundles
+{- | Encode an 'Nrt' score.
 
-{- | Write an 'Nrt' score.
-
-> import Sound.Osc {\- hosc -\}
-> import Sound.Sc3 {\- hsc3 -\}
+> import Sound.Osc
+> import qualified Sound.Sc3 as Sc3
+> import qualified Data.Digest.Murmur64 as Hash
 > m1 = g_new [(1, AddToTail, 0)]
 > m2 = d_recv (synthdef "sin" (out 0 (sinOsc ar 660 0 * 0.15)))
 > m3 = s_new "sin" 100 AddToTail 1 []
 > m4 = n_free [100]
 > m5 = nrt_end
 > sc = Nrt [bundle 0 [m1,m2],bundle 1 [m3],bundle 10 [m4],bundle 15 [m5]]
-> writeNrt "/tmp/t.osc" sc
+> let b = encodeNrt sc
+> let d = decodeNrt b
+> Hash.hash64 b
+Hash64 0x59f65af6bb615c3c
+
+> sc == d
+True
 -}
+encodeNrt :: Nrt -> ByteString.Lazy.ByteString
+encodeNrt = ByteString.Lazy.concat . map oscWithSize . nrt_bundles
+
+{- | Write an 'Nrt' score. -}
 writeNrt :: FilePath -> Nrt -> IO ()
-writeNrt fn = B.writeFile fn . encodeNrt
+writeNrt fn = ByteString.Lazy.writeFile fn . encodeNrt
 
 -- | Write an 'Nrt' score to a file handle.
 putNrt :: Handle -> Nrt -> IO ()
-putNrt h = B.hPut h . encodeNrt
+putNrt h = ByteString.Lazy.hPut h . encodeNrt
 
--- | Decode an 'Nrt' 'B.ByteString' to a list of 'Bundle's.
-decode_nrt_bundles :: B.ByteString -> [BundleOf Message]
+-- | Decode an 'Nrt' 'ByteString.Lazy.ByteString' to a list of 'Bundle's.
+decode_nrt_bundles :: ByteString.Lazy.ByteString -> [BundleOf Message]
 decode_nrt_bundles s =
-  let (p, q) = B.splitAt 4 s
+  let (p, q) = ByteString.Lazy.splitAt 4 s
       n = fromIntegral (Byte.decode_i32 p)
-      (r, s') = B.splitAt n q
+      (r, s') = ByteString.Lazy.splitAt n q
       r' = Decode.decodeBundle r
-  in if B.null s'
+  in if ByteString.Lazy.null s'
       then [r']
       else r' : decode_nrt_bundles s'
 
--- | Decode an 'Nrt' 'B.ByteString'.
-decodeNrt :: B.ByteString -> Nrt
+-- | Decode an 'Nrt' 'ByteString.Lazy.ByteString'.
+decodeNrt :: ByteString.Lazy.ByteString -> Nrt
 decodeNrt = Nrt . decode_nrt_bundles
 
-{- | 'decodeNrt' of 'B.readFile'.
-
-> readNrt "/tmp/t.osc"
--}
+{- | 'decodeNrt' of 'ByteString.Lazy.readFile'. -}
 readNrt :: FilePath -> IO Nrt
-readNrt = fmap decodeNrt . B.readFile
+readNrt = fmap decodeNrt . ByteString.Lazy.readFile
 
 -- * Query
 
